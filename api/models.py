25900f55411a6b52760e4910f8ac15e825105f84
Ousama Elayan <ousamasama@gmail.com>
2019-02-21 09:23:39 -0600

    added related names

--- api/models.py
+++ api/models.py
@@ -30,7 +30,7 @@ class ProductType(models.Model):
     name = models.CharField(max_length=100, blank=False)
 
     def __str__(self):
-        '''string method returns producttype name'''
+        """string method returns producttype name"""
         return self.name
 
     class Meta:
@@ -38,17 +38,17 @@ class ProductType(models.Model):
 
 # Product Model
 class Product(models.Model):
-    '''An item that a User can Sell or Buy'''
+    """An item that a User can Sell or Buy"""
     # customer in this instance is the product seller
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField(blank=False, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
     quantity = models.IntegerField(blank=False)
-    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, null=True)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, null=True, related_name='products')
 
     def __str__(self):
-        '''string method that returns the product name'''
+        """string method that returns the product name"""
 
         return self.name
 
@@ -57,14 +57,14 @@ class Product(models.Model):
 
 # PaymentType Model
 class PaymentType(models.Model):
-    '''A payment type saved by the buyer for use with orders'''
+    """A payment type saved by the buyer for use with orders"""
     # customer in this instance is the product buyer
     payment_name = models.CharField(max_length=50, blank=False)
     account_number = models.IntegerField(blank=False)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='payment_types')
 
     def __str__(self):
-        '''string method that returns the payment type name'''
+        """string method that returns the payment type name"""
 
         return self.customer.first_name + self.payment_name
         
@@ -73,25 +73,25 @@ class PaymentType(models.Model):
 
 # Order Model
 class Order(models.Model):
-    '''An order placed by the buying/logged in user'''
+    """An order placed by the buying/logged in user"""
     # customer in this instance is the product buyer
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    payment_type = models.ForeignKey(PaymentType, default=None, blank=True, null=True, on_delete=models.PROTECT)
-    product = models.ManyToManyField(Product, through='ProductOrder')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+    payment_type = models.ForeignKey(PaymentType, default=None, blank=True, null=True, on_delete=models.PROTECT, related_name='orders')
+    product = models.ManyToManyField(Product, through='ProductOrder', related_name='orders')
 
     def __str__(self):
-        '''string method that returns the Order id'''
+        """string method that returns the Order id"""
 
         return str(self.id)
 
 # ProductOrder Model
 class ProductOrder(models.Model):
-    '''A join table linking the product being sold to the order being placed'''
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    """A join table linking the product being sold to the order being placed"""
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='product_order')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='product_rder')
 
     def __str__(self):
-        '''string method that returns the ProductOrder id'''
+        """string method that returns the ProductOrder id"""
 
         return str(self.id)
 
@@ -100,13 +100,14 @@ class ProductOrder(models.Model):
 # -------------
 
 # Department Model
-class Department(models.Model):
+class Department(SafeDeleteModel):
     """a grouping of employees"""
+    _safedelete_policy = SOFT_DELETE_CASCADE
     department_name = models.CharField(max_length=100)
     budget = models.IntegerField()
 
     def __str__(self):
-        '''string method that returns the department name'''
+        """string method that returns the department name"""
 
         return self.department_name
 
@@ -123,7 +124,7 @@ class Employee(SafeDeleteModel):
     last_name = models.CharField(max_length=100)
     start_date = models.DateField('Starting Date')
     end_date = models.DateField('Ending Date', default=None, blank=True, null=True)
-    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, related_name='employees')
     is_supervisor = models.BooleanField(default=False)
 
     def __str__(self):
@@ -143,7 +144,7 @@ class Computer(SafeDeleteModel):
     manufacturer = models.CharField(max_length=30)
     model = models.CharField(max_length=30)
     is_available = models.BooleanField(default=True)
-    employee = models.ManyToManyField(Employee, through='Join_Computer_Employee')
+    employee = models.ManyToManyField(Employee, through='Join_Computer_Employee', related_name='computers')
 
     def __str__(self):
         computer_name = (f"{self.manufacturer} {self.model} - ID#{self.id}")
@@ -155,8 +156,8 @@ class Computer(SafeDeleteModel):
 # Join table for Computer & Employee
 class Join_Computer_Employee(models.Model):
     """a relationship between computers and employees"""
-    employee = models.ForeignKey('Employee', on_delete=models.PROTECT)
-    computer = models.ForeignKey('Computer', on_delete=models.PROTECT)
+    employee = models.ForeignKey('Employee', on_delete=models.PROTECT, related_name='employee_computers')
+    computer = models.ForeignKey('Computer', on_delete=models.PROTECT, related_name='employee_computers')
     assign_date = models.DateField('Assign Date')
     unassign_date = models.DateField('Unassign Date', default=None, blank=True, null=True)
 
@@ -170,7 +171,7 @@ class Training_Program(SafeDeleteModel):
     start_date = models.DateField('Starting Date')
     end_date = models.DateField('Ending Date')
     max_attendees =  models.IntegerField()
-    employee = models.ManyToManyField(Employee, through='Join_Training_Employee')
+    employee = models.ManyToManyField(Employee, through='Join_Training_Employee', related_name='training_programs')
 
     def __str__(self):
         """returns a training program name"""
@@ -179,5 +180,5 @@ class Training_Program(SafeDeleteModel):
 # Join table for Training Program & Employee
 class Join_Training_Employee(models.Model):
     """The join table for employees and training"""
-    employee = models.ForeignKey('Employee', on_delete=models.PROTECT)
-    training_program = models.ForeignKey('Training_Program', on_delete=models.PROTECT)
+    employee = models.ForeignKey(Employee, on_delete=models.PROTECT, related_name='employee_trainings')
+    training_program = models.ForeignKey(Training_Program, on_delete=models.PROTECT, related_name='employee_trainings')
