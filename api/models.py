9bf32e5fb7e958e484483824d050cb2a547d1072
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-02-22 15:24:01 -0600

    updated db and created a new json

--- api/models.py
+++ api/models.py
@@ -128,7 +128,7 @@ class Employee(SafeDeleteModel):
     is_supervisor = models.BooleanField(default=False)
 
     def __str__(self):
-        return_value = (f"{self.first_name} {self.last_name} works in the {self.department} department.  End date:{self.end_date}")
+        return_value = (f"{self.first_name} {self.last_name} works in the {self.department} department.")
         return return_value
     
     class Meta:
@@ -147,7 +147,7 @@ class Computer(SafeDeleteModel):
     employee = models.ManyToManyField(Employee, through='Join_Computer_Employee', related_name='computers')
 
     def __str__(self):
-        computer_name = (f"{self.manufacturer} {self.model} - ID#{self.id}: Available:{self.is_available}")
+        computer_name = (f"{self.manufacturer} {self.model} - ID#{self.id}")
         return computer_name
     
     class Meta:
